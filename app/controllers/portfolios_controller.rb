3b1adcc186985e35f85549aa0dc6fedfd995c606
Andy <anhdiep80@gmail.com>
2023-04-11 07:30:16 +0700

    Implemented show functionality for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,5 +37,9 @@ class PortfoliosController < ApplicationController
             format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
           end
         end
-      end
+    end
+
+    def show
+        @portfolio_item = Portfolio.find(params[:id])
+    end
 end
